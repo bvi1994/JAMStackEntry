3e5c92d5b7ef6aafa1c4713186aa0461ebdfe5ae
bvi1994 <bvi1@ucsc.edu>
2018-08-29 01:23:05 -0700

    Submission commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi everyone I am Brandon Vi and I would like a free ticket to FCC Hackathon. Best of luck to all!</h1>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
